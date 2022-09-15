5732e377db13813027abfd3c47e55d769907119c
SebastianoMV <sebastianovisco@gmail.com>
2022-09-15 15:46:07 +0200

    Codice finito senza bonus

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//prova
+string[] cibiPrefe = { "Nutella", "Tiramisù", "Pizza", "Gelato", "Panini", "Patatine Fritte", "Panzerotti", "Parmigiana"};
+
+
+Console.WriteLine("Ho "+ cibiPrefe.Length + " cibi preferiti");
+
+Console.WriteLine("E questa è la classifica:");
+
+for (int i = 0; i < cibiPrefe.Length; i++)
+{
+    Console.WriteLine(i + 1 + ") " + cibiPrefe[i]);
+}
+
+Console.WriteLine("Il mio cibo preferito è " + cibiPrefe[0]);
+Console.WriteLine("Il mio cibo non troppo preferito è " + cibiPrefe[cibiPrefe.Length - 1]);
+
